236727f58906a8ac86b7601c7023d8b6a36e5300
RomanAkhmetshyn <108425347+RomanAkhmetshyn@users.noreply.github.com>
2024-12-05 12:11:37 -0500

    Create mcmc_ttv_test.py

---
+++ mcmc_ttv_test.py
@@ -0,0 +1,8 @@
+import corner
+import batman as bm
+import numpy as np
+from matplotlib import pyplot as plt
+import time
+import matplotlib as mpl
+from tqdm import trange
+mpl.rcParams['figure.dpi'] = 300
